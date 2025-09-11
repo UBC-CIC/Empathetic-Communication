f2ba6ede48750d482c267757c09d267226cb205b
ssmubc <ssm.ubc@gmail.com>
2025-09-11 16:25:10 -0700

    Removed previous data migration process sql files

--- cdk/lambda/db_setup/migrations/add_user_token_limit.sql
+++
@@ -1,4 +0,0 @@
--- Add token limit and usage tracking to users table
-ALTER TABLE "users" 
-ADD COLUMN IF NOT EXISTS token_limit INTEGER DEFAULT 50000,
-ADD COLUMN IF NOT EXISTS tokens_used INTEGER DEFAULT 0;
